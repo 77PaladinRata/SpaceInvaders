07f5408e29015984f3144a9ac4438f95484e4542
DiegoSF9009 <soldadodecolorrosa@gmail.com>
2026-07-22 20:32:26 -0600

    Haciendo el menu
    creando armas cuadradas

--- Assets/Scrips/MobileAssetsManager.cs
+++ Assets/Scrips/MobileAssetsManager.cs
@@ -8,16 +8,35 @@ public class MobileAssetsManager : MonoBehaviour
     private GameObject[] desktopAssets;
     private void Awake()
     {
-        #if UNITY_IOS || UNITY_ANDROID
-           foreach (var asset in mobileAssets)
-           {
-                asset.SetActive(true);
-           }
-        #else
-            foreach (var asset in desktopAssets)
-            {
-                asset.SetActive(true);
-            }
-        #endif
+        if (Application.isEditor)
+        {
+            SetActiveAssets(desktopAssets, true);
+        }
+        else if (Application.isMobilePlatform)
+        {
+            SetActiveAssets(mobileAssets, true);
+        }
+        else
+        {
+            SetActiveAssets(desktopAssets, true);
+        }
+        ///* #if UNITY_IOS || UNITY_ANDROID
+           ///*foreach (var asset in mobileAssets)
+           ///*{
+                ///* SetActiveAvasset.SetActive(true);
+           ///*}
+        ///*#else
+            ///*foreach (var asset in desktopAssets)
+            ///*{
+                ///*asset.SetActive(true);
+            ///*}
+        ///*#endif
+    }
+    private void SetActiveAssets(GameObject[] assets, bool isActive)
+    {
+        foreach (var asset in assets)
+        {
+            asset.SetActive(isActive);
+        }
     }
 }
